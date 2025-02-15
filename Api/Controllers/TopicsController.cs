d7cedf917601a2e23aaf86e6bd54adf568778943
shirokikhdo <shirokikhdo@gmail.com>
2025-02-15 16:14:40 +0300

    Добавьте файлы проекта.

---
+++ Api/Controllers/TopicsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class TopicsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Hello() => 
+        await Task.FromResult(Ok(new { text = "hello" }));
+}
