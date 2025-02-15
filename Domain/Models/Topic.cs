0e90d53495043a2116bd61b1279abdfcb7a66a14
shirokikhdo <shirokikhdo@gmail.com>
2025-02-15 17:03:10 +0300

    Первая сущность

---
+++ Domain/Models/Topic.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+
+namespace Domain.Models;
+
+public class Topic : Entity<TopicId>
+{
+    public string Title { get; set; } = default!;
+    public DateTime? EventStart { get; set; } = default!;
+    public string Summary { get; set; } = default!;
+    public string TopicType { get; set; } = default!;
+    public Location Location { get; set; } = default!;
+
+    public static Topic Create(
+        TopicId topicId,
+        string title,
+        DateTime eventStart,
+        string summary,
+        string topicType,
+        Location location)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(title);
+        ArgumentException.ThrowIfNullOrEmpty(summary);
+        ArgumentException.ThrowIfNullOrEmpty(topicType);
+
+        var topic = new Topic
+        {
+            Id = topicId,
+            Title = title,
+            EventStart = eventStart,
+            Summary = summary,
+            TopicType = topicType,
+            Location = location
+        };
+        
+        return topic;
+    }
+}
