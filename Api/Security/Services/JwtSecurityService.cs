2c8f58c4607272c647cd862d6ec49daaeff1998b
shirokikhdo <shirokikhdo@gmail.com>
2025-02-17 22:24:26 +0300

    Jwt Security Service: реализация

--- Api/Security/Services/JwtSecurityService.cs
+++ Api/Security/Services/JwtSecurityService.cs
@@ -1,4 +1,8 @@
-﻿using Domain.Security;
+﻿using System.Security.Claims;
+using System.Text;
+using Domain.Security;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Security.Services;
 
@@ -13,7 +17,31 @@ public class JwtSecurityService : IJwtSecurityService
 
     public string CreateToken(CustomIdentityUser user)
     {
-        var secretKey = _configuration["AuthSettings:SecretKey"];
-        throw new NotImplementedException();
+        var secretKey = _configuration["AuthSettings:SecretKey"]!;
+
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.Name, user.UserName!),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+            new Claim("is_premium", "true")
+        };
+
+        var bytes = Encoding.UTF8.GetBytes(secretKey);
+        var key = new SymmetricSecurityKey(bytes);
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        var tokenHandler = new JsonWebTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            SigningCredentials = creds,
+            Subject = new ClaimsIdentity(claims),
+            IssuedAt = DateTime.UtcNow,
+            NotBefore = DateTime.UtcNow.AddMinutes(0),
+            Expires = DateTime.UtcNow.AddMinutes(1)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return token;
     }
 }
