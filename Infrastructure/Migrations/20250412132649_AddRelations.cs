b6e3399ff059ba66ba6f5f34e9cc8582c4884a2e
shirokikhdo <shirokikhdo@gmail.com>
2025-04-12 16:29:17 +0300

    Миграция

---
+++ Infrastructure/Migrations/20250412132649_AddRelations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Relationships",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TopicReference = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserReference = table.Column<string>(type: "TEXT", nullable: false),
+                    Role = table.Column<string>(type: "TEXT", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Relationships", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Relationships_AspNetUsers_UserReference",
+                        column: x => x.UserReference,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Relationships_Topics_TopicReference",
+                        column: x => x.TopicReference,
+                        principalTable: "Topics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_TopicReference",
+                table: "Relationships",
+                column: "TopicReference");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_UserReference",
+                table: "Relationships",
+                column: "UserReference");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Relationships");
+        }
+    }
+}
